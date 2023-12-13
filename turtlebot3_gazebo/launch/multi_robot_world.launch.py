d9b92850266e5b5ef9821bf123ecdcf0672dfadf
Hussein Ali Jaafar <husseinali.jaafar@gmail.com>
2023-12-13 08:30:24 -0500

    added map frame

--- turtlebot3_gazebo/launch/multi_robot_world.launch.py
+++ turtlebot3_gazebo/launch/multi_robot_world.launch.py
@@ -8,6 +8,9 @@ from launch.actions import ExecuteProcess
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from tf2_ros.static_transform_broadcaster import StaticTransformBroadcaster
+
 
 TURTLEBOT3_MODEL = os.environ['TURTLEBOT3_MODEL']
 
@@ -19,6 +22,39 @@ def generate_launch_description():
                          'worlds', world_file_name)
     launch_file_dir = os.path.join(get_package_share_directory('turtlebot3_gazebo'), 'launch')
     pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+    # static publisher between /map and /B01/odom
+    # static publisher between /map and /B02/odom
+    # static publisher between /map and /B03/odom
+    # static publisher between /map and /B04/odom
+
+    static_tf_node_B01 = Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            arguments=['1', '1', '0', '0', '0', '0', 'map', 'B01/odom'],
+            output='screen'
+        )
+
+    static_tf_node_B02 = Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            arguments=['-1', '1', '0', '0', '0', '0', 'map', 'B02/odom'],
+            output='screen'
+        )
+    
+    static_tf_node_B03 = Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            arguments=['-1', '-1', '0', '0', '0', '0', 'map', 'B03/odom'],
+            output='screen'
+        )
+    
+    static_tf_node_B04 = Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            arguments=['1', '-1', '0', '0', '0', '0', 'map', 'B04/odom'],
+            output='screen'
+        )
+    
 
     return LaunchDescription([
         IncludeLaunchDescription(
@@ -57,4 +93,9 @@ def generate_launch_description():
             PythonLaunchDescriptionSource([launch_file_dir, '/robot_state_publisher_m.launch.py']),
             launch_arguments={'use_sim_time': use_sim_time, 'robot_namespace': 'B04', 'frame_id' : 'B04/'}.items(),
         ),
+        static_tf_node_B01,
+        static_tf_node_B02,
+        static_tf_node_B03,
+        static_tf_node_B04,
     ])
+
